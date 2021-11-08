6676e0462d78ec1ce24adcc9440ac2ae09ee60eb
JerryZhang <zhangzhe941020@yahoo.com>
2021-11-08 15:29:34 -0800

    edit the register.ejs and add register function

--- reminderAppStarterFile/controller/auth_controller.js
+++ reminderAppStarterFile/controller/auth_controller.js
@@ -17,7 +17,15 @@ let authController = {
   }),
 
   registerSubmit: (req, res) => {
-    // implement
+    let registerObj = {
+      id: database.length + 1,
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password
+    }
+    database.push(registerObj)
+    console.log(database)
+    res.redirect("/login")
   },
 };
 
