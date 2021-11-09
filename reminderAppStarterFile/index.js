76b80d01f71bdba61fbecdc7862a3b1bf3eb6098
JerryZhang <zhangzhe941020@yahoo.com>
2021-11-09 13:56:42 -0800

    add router to the project

--- reminderAppStarterFile/index.js
+++ reminderAppStarterFile/index.js
@@ -4,6 +4,9 @@ const path = require("path"); //
 const ejsLayouts = require("express-ejs-layouts"); //
 const session = require("express-session") //
 
+const reminderRoute = require("./routes/reminderRoute")
+const authRoute = require("./routes/authRoute")
+
 app.use(express.static(path.join(__dirname, "public"))); //
 
 app.set("view engine", "ejs");
@@ -27,54 +30,8 @@ const passport = require("./middleware/passport"); //
 app.use(passport.initialize()); // use passport
 app.use(passport.session()); // use session
 
-app.use((req, res, next) => {
-  console.log(`User details are: `);
-  console.log(req.user);
-
-  console.log("Entire session object:");
-  console.log(req.session);
-
-  console.log(`Session details are: `);
-  console.log(req.session.passport);
-  next();
-});
-
-const reminderController = require("./controller/reminder_controller"); //
-
-const { ensureAuthenticated } = require("./middleware/checkAuth") //
-
-
-// Routes start here
-
-app.get("/reminder",ensureAuthenticated, reminderController.list);
-
-app.get("/reminder/new", reminderController.new);
-
-app.get("/reminder/:id", reminderController.listOne);
-
-app.get("/reminder/:id/edit", reminderController.edit);
-
-app.post("/reminder/", reminderController.create);
-
-// Implement this yourself
-app.post("/reminder/update/:id", reminderController.update);
-
-// Implement this yourself
-app.post("/reminder/delete/:id", reminderController.delete);
-
-const authController = require("./controller/auth_controller"); //
-const { forwardAuthenticated } = require("./middleware/checkAuth") //
-
-// Fix this to work with passport! The registration does not need to work, you can use the fake database for this.
-app.get("/register", authController.register);
-app.get("/login", forwardAuthenticated, authController.login);
-app.post("/register", authController.registerSubmit);
-app.post("/login", authController.loginSubmit);
-app.get("/logout", (req, res) => {
-  req.logout();
-  res.redirect("/login")
-})
-
+app.use("/reminder", reminderRoute);
+app.use("/auth", authRoute);
 
 app.listen(3001, function () {
   console.log(
